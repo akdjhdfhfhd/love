a29a84d50296c5562a98e86b9f27c04345cddacb
akdjhdfhfhd <125655757+akdjhdfhfhd@users.noreply.github.com>
2023-02-28 20:24:02 +0300

    Update channelplay.py

--- AnonX/utils/channelplay.py
+++ AnonX/utils/channelplay.py
@@ -3,7 +3,7 @@ from AnonX.utils.database import get_cmode
 
 
 async def get_channeplayCB(_, command, CallbackQuery):
-    if command == "c":
+    if command == "s":
         chat_id = await get_cmode(CallbackQuery.message.chat.id)
         if chat_id is None:
             try:
